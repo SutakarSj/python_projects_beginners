1b12f1837fc6f53bdc4c372763650c568c5c4334
SutakarSj <sjsutakar@gmail.com>
2025-07-01 21:42:51 +0530

    python project beginner

---
+++ here/zero width steganography.py
@@ -0,0 +1,31 @@
+﻿def encrypt(txt):
+    binary=''.join(format(ord(char), '08b') for char in txt)
+    mp={'0':'\u200C', '1': '\u200D'}
+    
+    ans=[]
+    for i in binary:
+        ans.append(mp[i])
+    f=open("dummy.txt","w")
+    f.write(binary)
+    f.close()
+    
+    hidden_txt=''.join(mp[bit] for bit in binary)
+    return hidden_txt , ans
+
+def decrypt(hidden):
+    mp={'\u200C':'0', '\u200D':'1'}
+    binary=''.join(mp[char] for char in hidden if char in mp)
+    
+    return ''.join(chr(int(binary [i:i+8],2)) for i in range (0,len(binary),8))
+
+def main():
+    text=input("Enter plain text :")
+    
+    hidden,ans=encrypt(text)
+    print("\nhidden:", hidden,"it is hidden the cipher text is below\n","\nhidden_value",ans)
+    
+    decoded=decrypt(hidden)
+    print("\ndecoded:", decoded)
+
+if __name__=="__main__":
+    main()
